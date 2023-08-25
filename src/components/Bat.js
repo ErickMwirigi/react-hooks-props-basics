54cf1bb091c0993bfdda2f14a46722513787e3b1
ErickMwirigi <erickmbae21@gmail.com>
2023-08-25 14:50:58 +0300

    Done Lab

---
+++ src/components/Bat.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+function Bat(props){
+    const B = "Erick";
+return (
+    <div>{props.Text} {B}</div>
+)
+
+}
+
+export default Bat;
--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,6 +1,7 @@
 import React from "react";
 import BlogContent from "./BlogContent";
 import Comment from "./Comment";
+import Bat from "./Bat";
 
 function BlogPost() {
   return (
@@ -9,6 +10,7 @@ function BlogPost() {
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+      <Bat Text="It's" No= {1} />
     </div>
   );
 }
